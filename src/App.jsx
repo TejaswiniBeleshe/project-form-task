400165aec4908680c5833e294ed74432582c3185
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-02-26 22:27:38 +0530

    Add client drop down

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 
 import './App.css'
 import FormSlide from './components/FormSlide/FormSlide.jsx'
+{
+  /* The following line can be included in your src/index.js or App.js file */
+}
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
--- src/components/Page1/Page1.jsx
+++ src/components/Page1/Page1.jsx
@@ -1,23 +1,26 @@
 import "./Page1.css"
+import Form from 'react-bootstrap/Form';
+import { useEffect, useState } from "react";
 const Page1 = function({dataOfPage1,setDataOfPage1}){
     const {projectName,notes,dates,client} = dataOfPage1
+    const [newClient,setNewClient] = useState('')
+    useEffect(()=>{
+         console.log(client)
+    },[client])
     return(
         <div className="page1">
-             <form >
+             <form>
                 <label htmlFor="project-name">Project name</label>
-                <input className="form-control" type="text" name="project-name" id="project-name" value={projectName} onChange={(e)=>} />
+                <input className="form-control" type="text" name="project-name" id="project-name" value={projectName} onChange={(e)=>setDataOfPage1({...dataOfPage1,projectName:e.target.value})} />
 
                 <label htmlFor="project-name">Client</label>
-                <div class="dropdown">
-                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown link
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                       <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </div>
+                <Form.Select aria-label="Default select example" value={client} onChange={(e)=>setDataOfPage1({...dataOfPage1,client:e.target.value})}>
+                    <option value="">Select Client</option>
+                    <option value="client1">Client1</option>
+                    <option value="client2">Client2</option>
+                    <option value="client3">Client3</option>
+                </Form.Select>
+                <input type="text" name="newClient" id="newClient" value={newClient} onChange={(e)=>setNewClient(e.target.value)} />
              </form>
         </div>
     )
