869abc081c2c71031757a260567c239a7ec39cf0
Tejaswini Belashe <sonu2201.beleshe@gmail.com>
2025-02-26 22:35:48 +0530

    Add formik validator

--- src/components/Page1/Page1.jsx
+++ src/components/Page1/Page1.jsx
@@ -1,6 +1,10 @@
 import "./Page1.css"
 import Form from 'react-bootstrap/Form';
 import { useEffect, useState } from "react";
+import {Formik,Form,ErrorMessage,Field} from "formik"
+import * as Yup from "yup"
+const today = new Date().toISOString().split("T")[0];
+
 const Page1 = function({dataOfPage1,setDataOfPage1}){
     const {projectName,notes,dates,client} = dataOfPage1
     const [newClient,setNewClient] = useState('')
@@ -9,6 +13,13 @@ const Page1 = function({dataOfPage1,setDataOfPage1}){
     },[client])
     return(
         <div className="page1">
+            <Formik initialValues={{ startDate: "", endDate: "" }}
+                validationSchema={validationSchema}
+                onSubmit={(values) => {
+                    alert(JSON.stringify(values, null, 2));
+                }}
+                >
+               {({ setFieldValue, values }) => (
              <form>
                 <label htmlFor="project-name">Project name</label>
                 <input className="form-control" type="text" name="project-name" id="project-name" value={projectName} onChange={(e)=>setDataOfPage1({...dataOfPage1,projectName:e.target.value})} />
@@ -22,6 +33,8 @@ const Page1 = function({dataOfPage1,setDataOfPage1}){
                 </Form.Select>
                 <input type="text" name="newClient" id="newClient" value={newClient} onChange={(e)=>setNewClient(e.target.value)} />
              </form>
+              )}
+            </Formik>
         </div>
     )
 }
